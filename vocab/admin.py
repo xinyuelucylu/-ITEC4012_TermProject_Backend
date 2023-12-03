4481870583279d41ac64fd97308cf8db3ffbaf32
mmll <xinyuelu2000@hotmail.com>
2023-12-03 15:50:26 -0500

    delete original files which are already in backend folder

--- vocab/admin.py
+++
@@ -1,6 +0,0 @@
-
-from django.contrib import admin
-from .models import Colors, Animals
-
-admin.site.register(Colors)
-admin.site.register(Animals)
--- vocabulary/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
-    path('', include('vocab.urls')),  # Include the app's URLs
-
-]
